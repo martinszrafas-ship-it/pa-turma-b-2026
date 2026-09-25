4fd8846b5069e0cbe6ee8879b30c7fd892958146
martinszrafas-ship-it <martinszrafas@gmail.com>
2026-09-25 11:40:12 -0300

    Delete 12_validador_de_voto (1).py

--- 12_validador_de_voto (1).py
+++
@@ -1,8 +0,0 @@
-idade = int(input("Digite sua idade: "))
-
-if idade < 16:
-    print("Não vota.")
-elif idade <= 17 or idade > 65:
-    print("Voto facultativo.")
-else:
-    print("Voto obrigatório.")
