df6386ace2d27ac7666ef732e8e0d7efb4bf9c7f
martinszrafas-ship-it <martinszrafas@gmail.com>
2026-09-25 11:38:45 -0300

    Delete 06_desconto_comercial (2).py

--- 06_desconto_comercial (2).py
+++
@@ -1,7 +0,0 @@
-valor = float(input("Digite o valor original do produto: R$ "))
-
-desconto = valor * 0.15
-valor_final = valor - desconto
-
-print(f"Valor do desconto: R$ {desconto:.2f}")
-print(f"Valor com desconto: R$ {valor_final:.2f}")
